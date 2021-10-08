71f3e38bf167fdca453c18e0eeaf8e81566ced3c
God-Slayer <60921940+God-Slayer@users.noreply.github.com>
2021-10-08 10:46:24 +0200

    Add download buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,9 +54,41 @@ $(document).ready(function(){
     },
     retina_detect: false
   });
-  var count_particles, update;
-  update = function (){
+  var update;
+  update=function (){
     requestAnimationFrame(update);
   };
   requestAnimationFrame(update);
+
+  $(document).on('click','.download_cont i',function(e){
+    e.stopPropagation();
+    if($(this).hasClass('open')){
+      $(this).removeClass('far');
+      $(this).addClass('fas');
+      $(this).removeClass('open');
+      $('.download_cont > span').removeClass('open');
+      $('.file_cont').removeClass('open');
+    }
+    else{
+      $(this).removeClass('fas');
+      $(this).addClass('far');
+      $(this).addClass('open');
+      $('.download_cont > span').addClass('open');
+      $('.file_cont').addClass('open');
+    }
+  });
+
+  $(document).on('click','.file_cont li',function(e){
+    e.stopPropagation();
+    $(this).find('a')[0].click();
+  });
+
+  $(document).on('click','body',function(){
+    console.log('called');
+    $('.download_cont i').removeClass('far');
+    $('.download_cont i').addClass('fas');
+    $('.download_cont i').removeClass('open');
+    $('.download_cont > span').removeClass('open');
+    $('.file_cont').removeClass('open');
+  });
 });
